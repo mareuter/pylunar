57e221aa16a42b3beb8d0beecac5a20aaed2eb1a
Michael Reuter <mareuternh@gmail.com>
2016-06-14 20:58:51 -0700

    Updating LunarFeature.

--- pylunar/lunar_feature.py
+++ pylunar/lunar_feature.py
@@ -5,6 +5,8 @@
 # ------------------------------------------------------------------------------
 import os
 
+__all__ = ["LunarFeature"]
+
 
 class LunarFeature(object):
 
--- tests/test_lunar_feature.py
+++ tests/test_lunar_feature.py
@@ -11,3 +11,6 @@ class TestLunarFeature(object):
         assert lf.name == "Clavius"
         assert lf.latitude == -58.622830884248
         assert lf.lunar_club_type == "Binocular"
+
+        val = str(lf)
+        assert val.startswith("Name")
