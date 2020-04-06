b3f5ac79a7839c725d28aea21ff5e70922c0d39f
Michael Reuter <mareuternh@gmail.com>
2020-04-05 20:19:23 -0700

    Update copyright date in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Python Lunar'
-copyright = u'2016, Michael Reuter'
+copyright = u'2016-2020, Michael Reuter'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
