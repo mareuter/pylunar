5d86fc77c0b13414375326d869df5467bd55db66
Michael Reuter <mareuternh@gmail.com>
2020-04-07 21:30:50 -0700

    Change version number.

--- pylunar/__init__.py
+++ pylunar/__init__.py
@@ -10,7 +10,7 @@
 
 __author__ = 'Michael Reuter'
 __email__ = 'mareuternh@gmail.com'
-__version__ = '0.5.1'
+__version__ = '0.6.0'
 
 from .helpers import *
 from .lunar_feature import LunarFeature
