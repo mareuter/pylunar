3b99b9a4d9408d765dc71d41c58a72d54a7364cd
Michael Reuter <mareuternh@gmail.com>
2020-04-07 21:01:06 -0700

    Remove unneeeded __future__ import.

--- pylunar/moon_info.py
+++ pylunar/moon_info.py
@@ -8,7 +8,6 @@
 # Use of this source code is governed by a 3-clause BSD-style
 # license that can be found in the LICENSE file.
 
-from __future__ import division
 from datetime import datetime
 from enum import Enum
 import math
